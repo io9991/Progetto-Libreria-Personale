b51cd59ab72d9b508265b51076ad79d5015f7fb9
io9991 <francyvaccaro2003@gmail.com>
2025-05-25 19:19:54 +0200

    corretto e aggiunto feature per la gui

--- src/main/java/gui/HomeForm.java
+++ src/main/java/gui/HomeForm.java
@@ -19,18 +19,19 @@ public class HomeForm extends Form{
     private void addGuiComponent(){
 
         //nella parte superiore della schermata voglio che ci sia il titolo e la barra di ricerca
-        JPanel superiore = new JPanel(new BorderLayout());
+        JPanel superiore = new JPanel(new BorderLayout(10, 0));
         superiore.setBackground(Common_constants.colore_primario);
+        superiore.setBorder(BorderFactory.createEmptyBorder(15,20,15,20));
         //il titolo lo metto in una label
         JLabel titolo = new JLabel("Personal Library");
         //camnio colore del font e tipologia
         titolo.setForeground(Common_constants.colore_font_titoli);
         titolo.setFont(new Font("Dialog", Font.BOLD, 30));
-
+        //alto a sinistra
         superiore.add(titolo, BorderLayout.WEST);
         //aggiungo la barra di ricerca
-        JTextField ricerca = new JTextField();
-        ricerca.setPreferredSize(new Dimension(300, 30));
+        JTextField ricerca = new JTextField(20);
+        ricerca.setPreferredSize(new Dimension(300, 35));
         ricerca.setBackground(Common_constants.colore_secondario);
         ricerca.setForeground(Common_constants.colore_font_titoli);
         ricerca.setFont(new Font("Dialog", Font.PLAIN, 15));
@@ -44,18 +45,64 @@ public class HomeForm extends Form{
 
 
         //passiamo alla parte centrale dove mettiamo la griglia per i libri
-        JPanel griglia = new JPanel(new GridLayout(6,6,10,10));
-        griglia.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        JPanel griglia = new JPanel();
+        griglia.setLayout(new GridLayout(0,2,20,20));
+        griglia.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         griglia.setBackground(Common_constants.colore_primario);
-        griglia.add(new PanelLibro("Titolo", "Autore", "codice", "Stato"));
-        griglia.add(new PanelLibro("Titolo2", "Autore2", "codice2", "Stato2"));
-        griglia.add(new PanelLibro("Titolo2", "Autore2", "codice2", "Stato2"));
+        //metto la barra per scorrere
+        JScrollPane barraScorrimento = new JScrollPane(griglia);
+        //quando serve appare
+        barraScorrimento.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        barraScorrimento.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        barraScorrimento.setBorder(BorderFactory.createEmptyBorder());
+        griglia.add(new PanelLibro("Titolo", "Autore", "codice", "Stato", 4));
+
+        add(barraScorrimento, BorderLayout.CENTER);
 
         JScrollPane centrale = new JScrollPane(griglia);
+      //  centrale.add(barraScorrimento, BorderLayout.CENTER);
         getContentPane().add(centrale, BorderLayout.CENTER);
+
+        //pannello a destra con bottone per aggiungere
+        //filtrare e ordinare
+
+        JPanel latoOperazioni = new JPanel();
+        latoOperazioni.setLayout(new BoxLayout(latoOperazioni, BoxLayout.Y_AXIS));
+        latoOperazioni.setBorder(BorderFactory.createEmptyBorder(20,10,20,20));
+        latoOperazioni.setBackground(Common_constants.colore_primario);
+
+        //bottone add, filtro e sort
+        JButton aggiungi = bottonePersonalizzato("Aggiungi");
+        JButton filtra = bottonePersonalizzato("Filtra");
+        JButton ordina = bottonePersonalizzato("Ordina");
+
+        latoOperazioni.add(Box.createVerticalGlue());
+        latoOperazioni.add(aggiungi);
+        latoOperazioni.add(Box.createRigidArea(new Dimension(0,15)));
+        latoOperazioni.add(filtra);
+        latoOperazioni.add(Box.createRigidArea(new Dimension(0,15)));
+        latoOperazioni.add(ordina);
+        latoOperazioni.add(Box.createVerticalGlue());
+
+        getContentPane().add(latoOperazioni, BorderLayout.EAST);
 
     }
 
-
+    //metodo che serve per creare un bottone personalizzato
+    private JButton bottonePersonalizzato(String testo){
+        JButton bottone = new JButton(testo);
+        bottone.setAlignmentX(Component.CENTER_ALIGNMENT);
+        bottone.setBackground(Common_constants.colore_secondario);
+        bottone.setForeground(Common_constants.colore_font_titoli);
+        bottone.setFont(new Font("Arial", Font.BOLD, 14));
+        bottone.setPreferredSize(new Dimension(100, 40));
+        bottone.setMaximumSize(new Dimension(150,40));
+        bottone.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(Common_constants.colore_secondario, 1),
+                BorderFactory.createEmptyBorder(5,15,5,15)
+        ));
+        bottone.setFocusPainted(false);
+        return bottone;
+    }
 
 }
--- src/main/java/gui/PanelLibro.java
+++ src/main/java/gui/PanelLibro.java
@@ -7,32 +7,62 @@ import java.awt.*;
 
 public class PanelLibro extends JPanel {
 
-    public PanelLibro(String titolo, String autore, String isbn, String statoLettura) {
-        setLayout(new BorderLayout());
-        setPreferredSize(new Dimension(30, 30)); // box fisso
+    private static final int larghezza = 200;
+    private static final int altezza = 250;
 
+    private int valutazione;
+
+    public PanelLibro(String titolo, String autore, String isbn, String statoLettura, int valutazione) {
+
+        //salvo valutazione
+        this.valutazione = valutazione;
+
+        setLayout(new BorderLayout(5,5));
+        setPreferredSize(new Dimension(larghezza, altezza)); // box fisso
+        setMinimumSize(new Dimension(larghezza, altezza));
+        setMaximumSize(new Dimension(larghezza, altezza));
         // Bordo per distinguere
-        setBorder(BorderFactory.createLineBorder(Common_constants.colore_font_titoli, 1));
+        setBorder(BorderFactory.createLineBorder(Common_constants.colore_font_titoli, 2));
         setBackground(Common_constants.colore_secondario);
+
+        //pannello informazioni principali
+        //utilizzo un box
+        JPanel info = new JPanel();
+        info.setLayout(new BoxLayout(info, BoxLayout.Y_AXIS));
+        info.setOpaque(false);
+        info.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10));
 
         // Titolo (in alto)
         JLabel titoloLabel = new JLabel("<html><b>" + titolo + "</b></html>");
-        titoloLabel.setFont(new Font("SansSerif", Font.BOLD, 14));
-        add(titoloLabel, BorderLayout.NORTH);
+        titoloLabel.setForeground(Common_constants.colore_font_titoli);
+        titoloLabel.setFont(new Font("Arial", Font.BOLD, 14));
+        titoloLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
 
-        // Dettagli centrali
-        JPanel dettagli = new JPanel(new GridLayout(2, 1));
-        dettagli.setOpaque(false);
-        dettagli.add(new JLabel("Autore: " + autore));
-        dettagli.add(new JLabel("ISBN: " + isbn));
-        add(dettagli, BorderLayout.CENTER);
 
-        // Stato lettura (in basso)
-        JLabel stato = new JLabel(statoLettura);
-        stato.setHorizontalAlignment(JLabel.RIGHT);
-        stato.setForeground(Common_constants.colore_font_titoli);
-        stato.setFont(new Font("SansSerif", Font.BOLD, 12));
-        add(stato, BorderLayout.SOUTH);
+        JLabel autoreLabel = new JLabel("Autore" + autore );
+        autoreLabel.setForeground(Common_constants.colore_font_titoli);
+        autoreLabel.setFont(new Font("Arial", Font.PLAIN, 12));
+        autoreLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        //add(titoloLabel, BorderLayout.NORTH);
+
+        JLabel isbnLabel = new JLabel("ISBN" + isbn );
+        autoreLabel.setForeground(Common_constants.colore_font_titoli);
+        autoreLabel.setFont(new Font("Arial", Font.PLAIN, 12));
+        autoreLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        info.add(titoloLabel);
+        info.add(Box.createVerticalStrut(5));
+        info.add(autoreLabel);
+        info.add(Box.createVerticalStrut(5));
+        info.add(isbnLabel);
+        //spingo i componenti verso l'alto
+        info.add(Box.createVerticalGlue());
+
+        add(info, BorderLayout.CENTER);
+
+        //parte inferiore del pannello  todo
+
+
     }
 
 }
