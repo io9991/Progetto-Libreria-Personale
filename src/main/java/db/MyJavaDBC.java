895f993cadd5400974ca7ec7a3d49da290a26e05
io9991 <francyvaccaro2003@gmail.com>
2025-05-28 00:25:14 +0200

    implementazione del singleton per il db

---
+++ src/main/java/db/MyJavaDBC.java
@@ -0,0 +1,61 @@
+package db;
+
+//ho deciso di implementare questa classe usando il dp SINGLETON
+
+//importo quello che mi serve
+import java.sql.*;
+
+import constants.Common_constants;
+import constants.Common_constants.*;
+
+public class MyJavaDBC {
+
+    private static MyJavaDBC instance = null;
+    private Connection connection;
+
+    //costruttore vuoto
+    private MyJavaDBC(){
+
+        try{
+            this.connection = DriverManager.getConnection(Common_constants.DB_URL, Common_constants.DB_USERNAME, Common_constants.DB_PASSWORD);
+            System.out.println("Connessione al database stabilita con successo.");
+        }catch(SQLException e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public static synchronized MyJavaDBC getInstance(){
+        if(instance == null){
+            instance = new MyJavaDBC();
+        }
+        return instance;
+    }
+
+    public Connection getConnection(){
+
+        try{
+            if(connection == null || connection.isClosed()){
+                System.out.println("connessione persa o chiusa");
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    //chiudiamo la connessione
+    public void closeConnection(){
+        if (this.connection != null){
+            try{
+                this.connection.close();
+                this.connection = null;
+            }catch(SQLException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+}
